d51e71d4ba24b1ab4c32b032c267c2a02b05b3d3
Cleberson Bieleski <clebersonbieleski@gmail.com>
2018-04-13 15:04:15 -0300

    atualização para matar sessions, para session_register_shutdown antes de um novo start

--- config/construction/Init.php
+++ config/construction/Init.php
@@ -4,11 +4,7 @@
  * @Author: Cleberson Bieleski
  * @Date:   2017-12-23 04:54:45
  * @Last Modified by:   Cleber
-<<<<<<< HEAD
- * @Last Modified time: 12-04-2018 17:13:51
-=======
- * @Last Modified time: 11-04-2018 20:59:58
->>>>>>> 9cfa596da69e4cfdb8db0ef6893786a33f85b5d7
+ * @Last Modified time: 12-04-2018 17:00:57
  */
 
 namespace DwPhp;
@@ -102,6 +98,7 @@ class Init{
 	public  $methodsURI;
 
 	function __construct($type=''){
+		
 		if($type=='test'){
 			return false;
 		}
@@ -676,7 +673,7 @@ class Init{
 			}while(!$dir);
 		}else{
 			$directory_ctrl = $this->getPathApplication().'controllers';
-			$directory_view = $this->getPathApplication().'views/pages/default';
+			$directory_view = $this->getPathApplication().'views/pages';
 			$this->urlCompletePath = substr($this->getPathBaseHref(),0,-1);
 			do{
 				if( (file_exists($directory_view) && in_array(current($url_array), scandir($directory_view))) || (file_exists($directory_ctrl) && in_array(current($url_array), scandir($directory_ctrl))) ){
