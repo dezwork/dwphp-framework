e953071684f52afb081512ee452f91c3cb4e4d3f
Cleberson Bieleski <clebersonbieleski@gmail.com>
2018-04-20 10:06:36 -0300

    update abstract class

--- library/models/AbstractObject.php
+++ library/models/AbstractObject.php
@@ -4,7 +4,7 @@
  * @Author: Cleberson Bieleski
  * @Date:   2017-12-23 04:54:45
  * @Last Modified by:   Cleber
- * @Last Modified time: 19-04-2018 21:41:46
+ * @Last Modified time: 20-04-2018 10:06:19
  */
 
 	namespace DwPhp\Library\models;
@@ -31,19 +31,14 @@
 	        if(!empty($order)){
 	            $db->setOrderBy($order);
 	        }
-
 	        if(!empty($limit)){
 	            $db->setLimit($limit);
 	        }
-
 	        if(count($search) > 0){
 	            $where = $this->getSearchQuery($db->getWhere(), $search);
-
 	            $db->setWhere($where);
 	        }
-
 	        $db->Select();
-
 	        $child = get_called_class();
 
 	        $return = array();
@@ -85,53 +80,36 @@
 	    }
 
     	public function insert($debug = false){
-	        $db = new sql();
 
 	        $params_tmp = get_object_vars($this);
-	        unset($params_tmp['dbTable']);
-	        unset($params_tmp['dateUpdate']);
-	        unset($params_tmp['userUpdate']);
+	        if(isset($params_tmp['dbTable'])){ unset($params_tmp['dbTable']);}
+	        if(isset($params_tmp['dateUpdate'])){ unset($params_tmp['dateUpdate']);}
+	        if(isset($params_tmp['userUpdate'])){ unset($params_tmp['userUpdate']);}
 
-	        $params_tmp = array_filter($params_tmp, function($var){ return !is_null($var);} );
+	        $params = array_filter($params_tmp, function($var){ return !is_null($var);} );
 
-	        $params = array();
-	        foreach($params_tmp as $i => $param){
-	            $i = systemFunctions::fromCamelCase($i);
-	            $params[$i] = $param;
-	        }
-
-	        if($debug === true){
-	            $db->setTest(true);
-	        }
+	        $db = new sql();
+	        $db->setTest($debug);
 	        $db->setTable($this->dbTable);
 	        $db->setSet($params);
 	        $db->Insert();
-
 	        $this->id = $db->getInsertId();
+
 	        return $this->id;
 	    }
 
 	    public function update($debug = false){
-	        $db = new sql();
 
 	        $params_tmp = get_object_vars($this);
 
-	        unset($params_tmp['dbTable']);
-	        unset($params_tmp['dateCreate']);
-	        unset($params_tmp['userCreate']);
+	        if(isset($params_tmp['dbTable'])){ unset($params_tmp['dbTable']);}
+	        if(isset($params_tmp['dateCreate'])){ unset($params_tmp['dateCreate']);}
+	        if(isset($params_tmp['userCreate'])){ unset($params_tmp['userCreate']);}
 
-	        // $params_tmp = array_diff($params_tmp, array(NULL));
-	        $params_tmp = array_filter($params_tmp, function($var){ return !is_null($var);} );
+			$params = array_filter($params_tmp, function($var){ return !is_null($var);} );
 
-	        $params = array();
-	        foreach($params_tmp as $i => $param){
-	            $i = systemFunctions::fromCamelCase($i);
-	            $params[$i] = $param;
-	        }
-
-	        if($debug === true){
-	            $db->setTest(true);
-	        }
+	        $db = new sql();
+	        $db->setTest($debug);
 	        $db->setTable($this->dbTable);
 	        $db->setSet($params);
 	        $db->setWhere(array('id' => $this->getId()));
@@ -142,26 +120,14 @@
 
     	public function delete($id){
 	        $db = new sql();
-
 	        $db->setTable($this->dbTable);
 	        $db->setWhere(array('id' => $id));
 	        $db->Delete();
 
 	        $query = $db->getLastQuery();
 
-	    //    $this->saveLog('DELETE', $query);
-
 	        return true;
 	    }
-
-	    //public function saveLog($action, $query){
-	    //	$logParams = array('action' => $action, 'query' => str_replace("'", '"', $query), 'owner' => $this->dbTable, 'date' => date("Y-m-d H:i:s"));
-	    //	if(isset($_SESSION['USER']['ID']) && !empty($_SESSION['USER']['ID']))
-	    //		$logParams['idUser'] = $_SESSION['USER']['ID'];
-	    //    $log = new Log();
-	    //    $log->insert($logParams);
-	    //}
-
 
 	    public function setId($id){
 	        $this->id = (int)$id;
