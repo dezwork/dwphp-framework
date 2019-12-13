7b24fad316fb019e41677ff6564fb16d120990a8
dezwork <dezwork.digital@gmail.com>
2019-12-13 13:35:57 -0300

    add remove dir

--- config/construction/Init.php
+++ config/construction/Init.php
@@ -242,7 +242,7 @@ class Init{
                     }else if($this->getEnvironmentStatus()=='testing' || $this->getEnvironmentStatus()=='development'){
                     	$dir_path = 'dev';
                     	if(file_exists(PATH_ROOT.'/app/dev/') && file_exists(PATH_ROOT.'/app/prod/')){
-                    		unlink(file_exists(PATH_ROOT.'/app/dev/'));
+                    		$this->deleteDir(PATH_ROOT.'/app/dev/');
                     	}
                     }
 
@@ -329,6 +329,27 @@ class Init{
 
 		$this->setConnectionDb($app_config);
 
+	}
+
+	public function deleteDir($dirPath) {
+	    if (is_dir($dirPath)) {
+	        
+		    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+		        $dirPath .= '/';
+		    }
+		    $files = glob($dirPath . '*', GLOB_MARK);
+		    foreach ($files as $file) {
+		        if (is_dir($file)) {
+		            $this->deleteDir($file);
+		        } else {
+		            unlink($file);
+		        }
+		    }
+		    rmdir($dirPath);
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 	// realiza as configurações do sistema
