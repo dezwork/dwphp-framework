b54b082b22beafcba6c2ea2337725bdb86b4c8b8
dezwork <dezwork.digital@gmail.com>
2019-12-13 13:41:17 -0300

    deletedir update

--- config/construction/Init.php
+++ config/construction/Init.php
@@ -347,21 +347,23 @@ class Init{
 	}
 
 	public function deleteDir($dirPath) {
-	    if (! is_dir($dirPath)) {
-	        throw new InvalidArgumentException("$dirPath must be a directory");
-	    }
-	    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-	        $dirPath .= '/';
-	    }
-	    $files = glob($dirPath . '*', GLOB_MARK);
-	    foreach ($files as $file) {
-	        if (is_dir($file)) {
-	            $this->deleteDir($file);
-	        } else {
-	            unlink($file);
-	        }
-	    }
-	    rmdir($dirPath);
+	    if (is_dir($dirPath)) {
+		    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+		        $dirPath .= '/';
+		    }
+		    $files = glob($dirPath . '*', GLOB_MARK);
+		    foreach ($files as $file) {
+		        if (is_dir($file)) {
+		            $this->deleteDir($file);
+		        } else {
+		            unlink($file);
+		        }
+		    }
+		    rmdir($dirPath);
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 	// realiza as configurações do sistema
